7ca7cd96cd48dd1b27fe27cdc43c9f0dbf7c9e1e
yj.kim <yj.kim@lipis.kr>
2024-01-01 12:59:40 +0900

    2476.py : dice

--- 2476.py
+++ 2476.py
@@ -1,4 +1,4 @@
-n = int(input())
+'''n = int(input())
 if n > 1000 or n < 2:
     exit()
 arr = []
@@ -17,4 +17,36 @@ for i in range(n):
     elif a!=b and b!=c:
         arr.append(max(a,b,c)*100)
         
-print(max(arr))
+print(max(arr))'''
+def same(a,b,c):
+    if a==b and b==c:
+        return 3
+    elif (a==b and b!=c):
+        return 2
+    elif (a==c and a!=c):
+        return 2
+    elif (b==c and a!=b):
+        return 2
+    elif a!=b and b!=c:
+        return 1
+def number(a,b,c):
+    if a==b:
+        return a
+    elif a==c:
+        return a
+    elif b==c:
+        return b
+        
+result = []
+n = int(input())
+for i in range(n):
+    a,b,c = map(int,input().split())
+    if same(a,b,c)==3:
+        result.append(10000 + 1000 * a)
+    elif same(a,b,c)==2:
+        result.append(1000 + number(a,b,c)*100)
+    elif same(a,b,c)==1:
+        result.append( max(a,b,c) * 100)
+        
+print(max(result))
+        
